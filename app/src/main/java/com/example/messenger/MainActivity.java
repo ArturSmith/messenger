86001751d15ce34cd9485e91d670444d965bca95
Artur Smith <arthur9smith7@gmail.com>
2023-05-21 12:43:17 +0200

    Implemented login activity. Created MainViewModel. Implemented login method and output of error text.

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private TextView loginButton;
+    private TextView forgotPasswordButton;
+    private TextView registerButton;
+    private TextView errorText;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        initViews();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = email.getText().toString().trim();
+                String passwordText = password.getText().toString().trim();
+                if (emailText.isEmpty() || passwordText.isEmpty()) {
+                    errorText.setVisibility(View.VISIBLE);
+                } else {
+                    errorText.setVisibility(View.GONE);
+                    viewModel.login(emailText, passwordText, MainActivity.this);
+                }
+            }
+        });
+
+    }
+
+    private void initViews() {
+        email = findViewById(R.id.editTextEmailAddress);
+        password = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+        forgotPasswordButton = findViewById(R.id.textViewForgotPassword);
+        registerButton = findViewById(R.id.textViewRegister);
+        errorText = findViewById(R.id.errorText);
     }
 }
---
+++ app/src/main/java/com/example/messenger/MainViewModel.java
@@ -0,0 +1,45 @@
+package com.example.messenger;
+
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+    }
+
+    public void login(String email, String password, Context context) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(context, "Authorized", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+}
